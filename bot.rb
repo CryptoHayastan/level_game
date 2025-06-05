d017003d77a4fbf47b592781de04d6063cbceaba
CryptoHayastan <crypto.hayastan@gmail.com>
2025-06-05 10:48:38 +0400

    +

--- bot.rb
+++ bot.rb
@@ -546,7 +546,7 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
           end
 
         when '/kap'
-          excluded_links = %w[HighKap]
+          excluded_links = %w[]
 
           shops_online = Shop.where(online: true).where.not(link: excluded_links)
           shops_offline = Shop.where(online: false).where.not(link: excluded_links)
@@ -807,7 +807,7 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
         when /^city_/
           bot.api.answer_callback_query(callback_query_id: update.id)
 
-          excluded_links = %w[HighKap]
+          excluded_links = %w[]
 
           city_id = update.data.split('_').last.to_i
           city = City.find_by(id: city_id)
