48e2a680efcc734585f2613c2bddc97128f399d5
CryptoHayastan <crypto.hayastan@gmail.com>
2025-07-15 20:57:42 +0400

    +

--- bot.rb
+++ bot.rb
@@ -633,7 +633,7 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
           end
 
         when '/kap'
-          excluded_links = %w[Enigmayvn KhalifastoryYVN HighKap Rick_Yerevan OperatorElina Diktatooooor PABL0NA PirateYVN]
+          excluded_links = %w[Enigmayvn KhalifastoryYVN HighKap Rick_Yerevan Diktatooooor PABL0NA PirateYVN]
 
           shops_online = Shop.where(online: true).where.not(link: excluded_links)
           shops_offline = Shop.where(online: false).where.not(link: excluded_links)
@@ -924,7 +924,7 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
         when /^city_/
           bot.api.answer_callback_query(callback_query_id: update.id)
 
-          excluded_links = %w[Enigmayvn KhalifastoryYVN HighKap Rick_Yerevan OperatorElina Diktatooooor PABL0NA PirateYVN]
+          excluded_links = %w[Enigmayvn KhalifastoryYVN HighKap Rick_Yerevan Diktatooooor PABL0NA PirateYVN]
 
           city_id = update.data.split('_').last.to_i
           city = City.find_by(id: city_id)
